fda4a98b54aa50c614f952f327064f8acd801317
nielsvissers <niels.vissers@telenet.be>
2020-12-24 13:42:57 +0100

    move outside mseccounter -> quicker processing inputs

--- hardware/I2C.cpp
+++ hardware/I2C.cpp
@@ -245,6 +245,10 @@ void I2C::Do_Work()
 	while (!IsStopRequested(100))
 	{
 		msec_counter++;
+		if (m_dev_type == I2CTYPE_MCP23017)
+		{
+			MCP23017_ReadChipDetails();
+		}
 		if (msec_counter == 10)
 		{
 			msec_counter = 0;
@@ -259,11 +263,7 @@ void I2C::Do_Work()
 					if (m_dev_type == I2CTYPE_PCF8574)
 					{
 						PCF8574_ReadChipDetails();
-					}
-					else if (m_dev_type == I2CTYPE_MCP23017)
-					{
-						MCP23017_ReadChipDetails();
-					}
+					} 
 				}
 				if (sec_counter % I2C_SENSOR_READ_INTERVAL == 0)
 				{
@@ -461,6 +461,7 @@ void I2C::MCP23017_ReadChipDetails()
 		_log.Log(LOG_NORM, "I2C::MCP23017_ReadChipDetails. %s. Failed to read from I2C device at address: 0x%x", szI2CTypeNames[m_dev_type], m_i2c_addr);
 		return; //read from i2c failed
 	}
+	_log.Log(LOG_NORM "0x%x", data.word);
 	/*if (data.word == 0xFFFF)
 	{ // if oidir port is 0xFFFF means the chip has been reset
 		_log.Log(LOG_NORM, "I2C::MCP23017_ReadChipDetails, Cur_iodir: 0xFFFF, call MCP23017_Init");
